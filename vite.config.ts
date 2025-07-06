fdce78b6e536ff349793e34e52996e85f4a1cc8a
Jeremy <jeremyl0603@linjiaweideMac-mini.local>
2025-07-06 15:09:39 +0800

    chore: 移除多餘的檔案、路由配置、CSS配置

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vue3-ball-animate/',
+  base: process.env.NODE_ENV === 'production' ? '/vue3-ball-animate/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
